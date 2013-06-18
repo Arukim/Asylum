7fe90cbeaf1539065113193b2addf993ed5d600c
Nikita Smelov <nikita@localhost.localdomain>
2013-06-18 20:05:28 +0400

    Added state machine prototype

--- asylum/asylum.go
+++ asylum/asylum.go
@@ -5,13 +5,25 @@ import (
 	"time"
 )
 
+const (
+	stateOffline = iota
+	stateConnected = iota
+	stateInGame = iota
+)
+
 type Bot struct{
 	Name string
+	State int
 }
 
 func (bot Bot) Born(message string, delay time.Duration){
 	for {
-		time.Sleep(delay)
-		fmt.Printf("%v is alive\r\n",bot.Name)
+		switch bot.State{
+		case stateOffline:
+			time.Sleep(delay)
+			fmt.Printf("%v is alive\r\n",bot.Name)
+		default:
+			time.Sleep(delay)
+		}			
 	}
 }
