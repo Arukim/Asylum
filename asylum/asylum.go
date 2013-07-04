902223ef20c58ff76b6b3f5a2fe011087c571170
Nikita Smelov <nikita@localhost.localdomain>
2013-07-05 00:43:35 +0400

    Massive remake of asylum split into handlers fixed for 0.1 server version (empty options in selectTurn)

--- asylum/asylum.go
+++ asylum/asylum.go
@@ -64,6 +64,8 @@ type Bot struct{
 	Name string
 	GamesCount int
 	State int
+	conn net.Conn
+	remoteAddr string
 }
 
 type LoginPacket struct{
@@ -102,88 +104,101 @@ func init(){
 //	fmt.Printf("%+v", CardsPool)
 }
 
+func hBotOffline(bot* Bot){
+	for bot.State == stateOffline {
+		_conn, err := net.Dial("tcp", bot.remoteAddr)
+		if err != nil {
+			//log.Println("Can't resolve server address")
+		}else{
+			bot.conn = _conn
+			bot.State = stateConnected;
+		}
+	}
+}
+
+func hBotConnected(bot* Bot){
+	for bot.State == stateConnected {
+		str, err := bufio.NewReader(bot.conn).ReadString('\n')
+		if err != nil {
+			log.Println("Can't read %v", err)
+			bot.State = stateOffline
+		}else{
+			var serverInfo ServerInfoPacket
+			err := json.Unmarshal([]byte(str), &serverInfo)
+			if err != nil{
+				log.Println("error:", err)
+				log.Println(str)
+			}else{
+				var packet LoginPacket
+				packet.Name = bot.Name
+				wr, _ := json.Marshal(packet)
+				_, _ = bot.conn.Write([]byte(wr))
+				bot.State = stateInGame
+			}
+		}
+	}
+}
+
+func makeTurn(bot* Bot, turnPacket * ServerTurnPacket) ClientTurnPacket{
+	var clientTurn ClientTurnPacket
+	turnCount := len(turnPacket.Options)
+	choosed := -1
+	for i,option := range turnPacket.Options{
+		if option.Type == "PLAY_ALL_TREASURES" {
+			choosed = i
+		}
+		if option.Type == "BUY" && choosed == -1 {
+			if option.Target == "Estate" {
+				choosed = i
+			}
+		}
+	}
+	if(choosed == -1){
+		clientTurn.OptionNumber = rand.Intn(turnCount)
+	}else{
+		clientTurn.OptionNumber = choosed
+	}
+	return clientTurn
+}
+
+func hBotInGame(bot* Bot){
+	bot.GamesCount++
+	bufRead := bufio.NewReader(bot.conn)
+	for bot.State == stateInGame {
+		str, err := bufRead.ReadString('\n')
+		if err != nil {
+			bot.State = stateOffline
+		}else{
+			var turnPacket ServerTurnPacket
+			err := json.Unmarshal([]byte(str), &turnPacket)
+			if err != nil {
+				log.Println("error:", err)
+				log.Println(str)
+			}else{
+				if len(turnPacket.Options) != 0 {
+					clientTurn := makeTurn(bot, &turnPacket)
+					wr, _ := json.Marshal(clientTurn)
+					_, _ = bot.conn.Write([]byte(wr))
+				}
+			}
+		}
+	}
+}
 
 func (bot Bot) Born(remoteAddr string, name string, uplink chan Bot){
 	bot.Name = name
-	uplink <- bot
-	var conn net.Conn
+	bot.remoteAddr = remoteAddr
 	for {
+		uplink <- bot
 		switch bot.State{
 		case stateOffline:
-			uplink <- bot
-			for bot.State == stateOffline {
-				_conn, err := net.Dial("tcp", remoteAddr)
-				if err != nil {
-					//log.Println("Can't resolve server address")
-				}else{
-					conn = _conn
-					bot.State = stateConnected;
-				}
-			}
+			hBotOffline(&bot)
 		case stateConnected:
-			uplink <- bot
-			for bot.State == stateConnected {
-				str, err := bufio.NewReader(conn).ReadString('\n')
-				if err != nil {
-					log.Println("Can't read %v", err)
-					bot.State = stateOffline
-				}else{
-					var serverInfo ServerInfoPacket
-					err := json.Unmarshal([]byte(str), &serverInfo)
-					if err != nil{
-						log.Println("error:", err)
-						log.Println(str)
-					}else{
-						var packet LoginPacket
-						packet.Name = bot.Name
-						wr, _ := json.Marshal(packet)
-						_, _ = conn.Write([]byte(wr))
-						bot.State = stateInGame
-					}
-				}
-			}
+			hBotConnected(&bot)
 		case stateInGame:
-			bot.GamesCount++
-			uplink <- bot
-			for bot.State == stateInGame {
-				str, err := bufio.NewReader(conn).ReadString('\n')
-				if err != nil {
-				//	log.Println("error:", err)
-					bot.State = stateOffline
-				//	time.Sleep(10*time.Millisecond)
-					
-				}else{
-					var turnPacket ServerTurnPacket
-					err := json.Unmarshal([]byte(str), &turnPacket)
-					if err != nil {
-						log.Println("error:", err)
-						log.Println(str)
-					}else{
-						turnCount := len(turnPacket.Options)
-						var clientTurn ClientTurnPacket
-						choosed := -1
-						for i,option := range turnPacket.Options{
-							if option.Type == "PLAY_ALL_TREASURES" {
-								choosed = i
-							}
-							if option.Type == "BUY" && choosed == -1 {
-								if option.Target == "Estate" {
-									choosed = i
-								}
-							}
-						}
-
-						if(choosed == -1){
-						clientTurn.OptionNumber = rand.Intn(turnCount)
-						}else{
-							clientTurn.OptionNumber = choosed
-						}
-							wr, _ := json.Marshal(clientTurn)
-							_, _ = conn.Write([]byte(wr))
-						}
-				}
-			}
+			hBotInGame(&bot)
 		default:
+			panic("Unknown state!");
 		}			
 	}
 }
